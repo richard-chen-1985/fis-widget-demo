68f3cd00e4edaf5c4180ef49edc6f78fb76c5756
richard chen <clq_web@126.com>
2015-08-20 15:06:12 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # fis-widget-demo
 fis 组件化demo
+
+# 目录结构
+<pre>
+root:
+  ├ _docs: 项目文档目录
+  ├ page: 页面文件
+  │  ├ home.html
+  │  ├ list.html
+  │  ├ detail.html
+  ├ sass: SASS库目录
+  ├ static: 静态资源目录
+  │  ├ lib: js库文件目录(jquery, zepto等)
+  │  ├ app: js目录
+  │  ├ css: css目录
+  │  ├ img: 图片目录
+  ├ test: 测试目录
+  └ widget: 组件目录
+     ├ header
+     │  ├ header.tpl
+     │  ├ header.js
+     │  ├ header.css
+     │  ├ header.json
+     └ nav
+        ├ nav.tpl
+        ├ nav.js
+        ├ nav.css
+        └ nav.json
+</pre>
--- fis-conf.js
+++ fis-conf.js
@@ -1,46 +1,76 @@
+// 模块引用
 var path = require('path');
 
+// 各种配置及开关
 var config = {
     // 静态资源版本号
     VERSION: '1.0.0',
     // 合并开关
     PACKED: true,
-    // cdn域名
-    CDN: '', //'http://static.360buyimg.com/yiye',
+    // cdn域名开关，prod环境始终为true
+    CDN: false,
+    // cdn域名地址
+    CDNURL: 'http://static.360buyimg.com/yiye',
     // MD5后缀开关
-    USEHASH: false
+    USEHASH: false,
+    // 模块化配置
+    amdMod: {
+        mode: 'amd',
+        forwardDeclaration: true,
+        baseUrl: '/static/app',
+        paths: {
+            dialogEx: 'common/dialogEx',
+            jdUser: 'common/jdUser',
+            jqueryPin: 'common/jquery.pin',
+            jqueryShare: 'common/jquery.share',
+            fixable: 'common/jquery.fixable',
+            beanExchange: 'common/bean.exchange',
+            clipboard: 'common/zeroclipboard-1.3.5/ZeroClipboard'
+        }
+    }
 }
 
-fis.hook('module', {
-    mode: 'amd',
-    forwardDeclaration: true,
-    baseUrl: '/static/app',
-    paths: {
-        dialogEx: 'common/dialogEx',
-        jdUser: 'common/jdUser',
-        jqueryPin: 'common/jquery.pin',
-        jqueryShare: 'common/jquery.share',
-        fixable: 'common/jquery.fixable',
-        beanExchange: 'common/bean.exchange',
-        clipboard: 'common/zeroclipboard-1.3.5/ZeroClipboard'
-    }
-})
+// 添加全局忽略
+fis.set('project.ignore', fis.get('project.ignore').concat([
+    '{_docs,sass)/**',
+    'page/**/_*.scss',
+    '{page,widget}/**.tpl',
+    '{page,widget}/**.json'
+]));
 
+// 开启模块化插件
+config.amdMod && fis.hook('module', amdMod);
+
+// 静态资源加载插件
 fis.match('::package', {
     postpackager: fis.plugin('loader', {
         useInlineMap: true
     })
 })
 
+// 合并设置
+config.PACKED && fis
+    .match('static/lib/**', {
+        packTo: 'static/lib/lib_pkg.js'
+    })
+    .match('static/app/{common.js,common/**.js}', {
+        packTo: 'static/app/common_pkg.js'
+    })
+    .match('static/css/**{css,scss}', {
+        packTo: 'static/css/common_pkg.css'
+    })
+    .match('widget/**.js', {
+        packTo: 'widget/widget_pkg.js'
+    })
+    .match('widget/**.{css,scss}', {
+        packTo: 'widget/widget_pkg.css'
+    })
+
 // 默认设置
 fis
     // 关闭useHash
-    .match('**', {
+    .match('*', {
         useHash: false
-    })
-    // 排除不需要发布文件
-    .match('{sass/**,widget/**.tpl,widget/**.json}', {
-        release: false
     })
     // 添加velocity模板引擎
     .match('page/**.html', {
@@ -59,29 +89,8 @@ fis
     })
     // CDN
     .match('*', {
-        domain: config.CDN != '' ? (config.CDN + '/' + config.VERSION) : config.CDN
+        domain: config.CDN ? (config.CDNURL + '/' + config.VERSION) : ''
     })
-
-
-// 合并设置
-if(config.PACKED) {
-    fis
-        .match('static/lib/**', {
-            packTo: 'static/lib/lib_pkg.js'
-        })
-        .match('static/app/{common.js,common/**.js}', {
-            packTo: 'static/app/common_pkg.js'
-        })
-        .match('static/css/**{css,scss}', {
-            packTo: 'static/css/common_pkg.css'
-        })
-        .match('widget/**.js', {
-            packTo: 'widget/widget_pkg.js'
-        })
-        .match('widget/**.{css,scss}', {
-            packTo: 'widget/widget_pkg.css'
-        })
-}
 
 // 生产测试环境设置
 // 和线上有同样MD5，用于调试线上BUG
@@ -90,7 +99,7 @@ fis
     .match('*.{css,scss,png,js}', {
         useHash: config.USEHASH
     })
-    .match('test/**', {
+    .match('{page,test}/**', {
         release: false
     })
     .match('*', {
@@ -106,7 +115,7 @@ fis
     .match('*.{css,scss,png,js}', {
         useHash: config.USEHASH
     })
-    .match('test/**', {
+    .match('{page,test}/**', {
         release: false
     })
     .match('*.js', {
@@ -121,6 +130,8 @@ fis
         optimizer: fis.plugin('png-compressor')
     })
     .match('*', {
+        // 发布环境始终开启CDN
+        domain: config.CDNURL + '/' + config.VERSION,
         deploy: fis.plugin('local-deliver', {
             to: 'output/prod/' + config.VERSION
         })
